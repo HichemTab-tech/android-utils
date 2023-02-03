886dc10468f172f37ed0a3e8c7cebe497ec0ce31
HichemTab <konanhichemsinshi@gmail.com>
2023-02-03 12:22:01 +0100

    add CodeQr utils

---
+++ android-utils/src/main/java/com/hichemtech/android_utils/CodeQR.java
@@ -0,0 +1,58 @@
+package com.hichemtech.android_utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.widget.ImageView;
+
+import androidx.annotation.DrawableRes;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+public class CodeQR {
+    public static void generateCodeQr(
+            Context context,
+            String content,
+            ImageView codeQrImage,
+            @DrawableRes int drawable
+    ) {
+        QRCodeWriter writer = new QRCodeWriter();
+        try {
+            BitMatrix bitMatrix = writer.encode(content, BarcodeFormat.QR_CODE, 512, 512);
+            int width = bitMatrix.getWidth();
+            int height = bitMatrix.getHeight();
+            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    bmp.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+                }
+            }
+            Bitmap yourLogo = BitmapFactory.decodeResource(context.getResources(), drawable);
+            codeQrImage.setImageBitmap(mergeBitmaps(yourLogo, bmp));
+
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Bitmap mergeBitmaps(Bitmap logo, Bitmap qrcode) {
+
+        Bitmap combined = Bitmap.createBitmap(qrcode.getWidth(), qrcode.getHeight(), qrcode.getConfig());
+        Canvas canvas = new Canvas(combined);
+        int canvasWidth = canvas.getWidth();
+        int canvasHeight = canvas.getHeight();
+        canvas.drawBitmap(qrcode, new Matrix(), null);
+
+        Bitmap resizeLogo = Bitmap.createScaledBitmap(logo, canvasWidth / 5, canvasHeight / 5, true);
+        int centreX = (canvasWidth - resizeLogo.getWidth()) /2;
+        int centreY = (canvasHeight - resizeLogo.getHeight()) / 2;
+        canvas.drawBitmap(resizeLogo, centreX, centreY, null);
+        return combined;
+    }
+}
